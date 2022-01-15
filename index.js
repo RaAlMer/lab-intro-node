3d4686acf85f954822a0523ccab05a3e5ce56502
Raúl <raul.alonso.merino@gmail.com>
2022-01-15 21:18:38 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,65 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    if (this.length === 0){
+      this.items.push(item);
+      this.length = 1;
+    } else {
+      this.items.push(item);
+      this.length += 1;
+      this.items.sort((a,b) => {
+        return a - b;
+      });
+    };    
+  };
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length){
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    };
+  };
 
-  max() {}
+  max() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[this.length - 1];
+    }
+  };
 
-  min() {}
+  min() {
+    if (this.length === 0){
+      throw new Error('EmptySortedList');
+    } else {
+      return this.items[0];
+    }
+  };
 
-  sum() {}
+  sum() {
+    if (this.length === 0){
+      return 0;
+    } else {
+      let sum = 0;
+      for(let i = 0; i < this.length; i++){
+        sum += this.items[i];
+      };
+      return sum;
+    };
+  };
 
-  avg() {}
-}
+  avg() {
+    if (this.length === 0){
+      throw new Error("EmptySortedList");
+    } else {
+      return this.sum()/this.length;
+    };
+  };
+};
 
 module.exports = SortedList;
